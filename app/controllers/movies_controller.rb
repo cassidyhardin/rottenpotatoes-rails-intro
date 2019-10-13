463adf0031e64952efe1c83c24add1aa9dc8a9e8
Cassidy Hardin <cassidyhardin@berkeley.edu>
2019-10-12 23:55:20 -0700

    checking

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :title_sorted, :release_sorted)
   end
 
   def show
